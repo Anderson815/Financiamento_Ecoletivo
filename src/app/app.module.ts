c569832618a03776531f5420b6fc8d3060c0452f
Caio Vitor <caio199307@gmail.com>
2021-05-28 14:58:16 -0300

    Add: Component equipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { EquipeComponent } from './equipe/equipe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RodapeComponent
+    RodapeComponent,
+    EquipeComponent
   ],
   imports: [
     BrowserModule,
