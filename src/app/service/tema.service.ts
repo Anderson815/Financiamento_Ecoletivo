498655ed5d9a4b0ee5c5182f49b10337a5ebb0d1
Anderson Correia de Souza <anderson_souza33@outlook.com.br>
2021-06-07 10:50:03 -0300

    feat: service de tema com os gets

---
+++ src/app/service/tema.service.ts
@@ -0,0 +1,30 @@
+import { Observable } from 'rxjs';
+import { environment } from './../../environments/environment.prod';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Tema } from '../model/Tema';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TemaService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
+
+  getAllTema(): Observable<Tema[]>{
+    return this.http.get<Tema[]>(`${environment.server}/tema`, this.token)
+  }
+
+  getByIdTema(id: number): Observable<Tema>{
+    return this.http.get<Tema>(`${environment.server}/tema/${id}`, this.token)
+  }
+
+  
+
+}
