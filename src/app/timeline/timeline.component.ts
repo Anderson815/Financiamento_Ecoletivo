8bf9cca5362d09b9f02a0648843f8d89aea5e89f
[Tamires] <thammisantos@hotmail.com>
2021-06-09 15:55:38 -0300

    fit: implementando get-postagem

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -1,3 +1,7 @@
+import { PostagemService } from './../service/postagem.service';
+import { User } from './../model/User';
+import { AuthService } from './../service/auth.service';
+import { Postagem } from './../model/Postagem';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
@@ -9,9 +13,17 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class TimelineComponent implements OnInit {
 
+  postagem: Postagem = new Postagem()
+  listaPostagens: Postagem[]
+
+  user: User = new User()
+  idUser = environment.id
+
   constructor(
     private router: Router,
     private route: ActivatedRoute,
+    private authService: AuthService,
+    private postagemService: PostagemService
   ) { }
 
   ngOnInit() {
@@ -20,6 +32,18 @@ export class TimelineComponent implements OnInit {
     if(environment.token == ""){
       this.router.navigate(['/entrar'])
     }
+
+    this.getAllPostagem()
   }
 
+getAllPostagem(){
+  this.postagemService.getAllPostagem().subscribe((resp: Postagem[]) => {
+    this.listaPostagens = resp
+  })
+}
+findByUser(){
+  this.authService.getByIdUser(this.idUser).subscribe((resp: User) =>{
+    this.user = resp
+  })
+}
 }
