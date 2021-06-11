6612f1c5df4bc5ba2f5dbeee409fb31ee23062d8
Anderson Correia de Souza <anderson_souza33@outlook.com.br>
2021-06-11 10:59:11 -0300

    fix: layout do modal de doação

--- src/app/doacao/doacao.component.ts
+++ src/app/doacao/doacao.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Postagem } from '../model/Postagem';
+import { AlertasService } from '../service/alertas.service';
+import { PostagemService } from '../service/postagem.service';
 
 @Component({
   selector: 'app-doacao',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DoacaoComponent implements OnInit {
 
-  constructor() { }
+  @Input() postagem: Postagem;
+  valor: number;
+
+  constructor(
+    private postagemService: PostagemService,
+    private router: Router,
+    private alertas: AlertasService
+  ) { }
 
   ngOnInit() {
+  }
+  
+  doar(){
+    this.postagemService.doarPostagem(this.postagem.id, this.valor).subscribe((resp: Postagem) => {
+      this.alertas.showAlertSucess("Doação realizada com sucesso!");
+      this.router.navigate(["/inicial"])
+      setTimeout(()=>{
+        this.router.navigate(["/timeline"])
+      },100)
+    })
+  }
+
+  cancelar(){
+    this.router.navigate(["/inicial"])
+    setTimeout(()=>{
+      this.router.navigate(["/timeline"])
+    },100)
   }
 
 }
